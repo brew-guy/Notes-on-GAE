0154b279e6a83166109a5d4e5ae44021b78b0754
jesh <jes.hunsballe@gmail.com>
2015-08-11 20:23:53 +0200

    Split stages in raw files and stripped html wrapping. Added html packing routine to wrap raw stage notess in html.

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ import cgi
 import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
   loader = jinja2.FileSystemLoader(template_dir),
   autoescape = True)
+
+LESSON_KEY, CONCEPT_KEY, CONCEPT_END = '// LESSON //', '// CONCEPT //', '// CONCEPT END //'
 
 class Handler(webapp2.RequestHandler):
   """Generic Handler; will be inherited by more specific path Handlers"""
@@ -44,7 +49,19 @@ class NotesHandler(Handler):
   def get(self):
     p = self.request.get('p')
     q = self.request.get('q','')
-    self.render("notes.html", p = p, q = q)
+    stage1_list = make_lesson_list(load_stage('templates/notes_stage1_raw.html'))
+    stage2_list = make_lesson_list(load_stage('templates/notes_stage2_raw.html'))
+    stage3_list = make_lesson_list(load_stage('templates/notes_stage3_raw.html'))
+    stage4_list = make_lesson_list(load_stage('templates/notes_stage4_raw.html'))
+    stage5_list = make_lesson_list(load_stage('templates/notes_stage5_raw.html'))
+    self.render("notes.html",
+                p = p,
+                q = q,
+                stage1 = stage1_list,
+                stage2 = stage2_list,
+                stage3 = stage3_list,
+                stage4 = stage4_list,
+                stage5 = stage5_list)
 
 class FizzBuzzHandler(Handler):
   def get(self):
@@ -93,3 +110,43 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/fizzbuzz', FizzBuzzHandler),
                                ('/sign', PostWall),
                               ], debug=True)
+
+def load_stage(file_name):
+  '''Loads lessons + concepts from a text/html course stage file'''
+  with open(file_name,'r') as file_input:
+    lines = file_input.readlines()
+  # lines will be a list that contains all of our lines.
+  # We use the String.join technique to join all of our elements in the list
+  return ''.join(lines)
+
+def make_lesson_list(text):
+  '''Takes the total lessons with concepts text, returns a nested list containing Lesson, Concept Title and Concept Description, [['L1', ['C1', 'D']], ['L2', ['C2', 'D']]]
+    MY GOD, IT'S CLUNKY!!!
+  '''
+  stage_list = []
+  lesson_list = []
+  lesson_ready = True
+  while text != '':
+    next_lesson_start = text.find(LESSON_KEY) + len(LESSON_KEY)+1
+    next_lesson_end = text.find(CONCEPT_KEY)-1
+    next_concept_start = text.find(CONCEPT_KEY) + len(CONCEPT_KEY)+1
+    next_concept_end = text.find(CONCEPT_END)-1
+    if (next_lesson_end >= 0 and lesson_ready == True):
+      lesson = text[next_lesson_start:next_lesson_end]
+      lesson_list.append(lesson)
+      text = text[next_lesson_end+1:]
+    elif next_concept_end >= 0:
+      concept_str = text[next_concept_start:next_concept_end]
+      concept_title = concept_str[:concept_str.find('\n')]
+      concept_body = concept_str[concept_str.find('\n')+1:]
+      concept_list = [concept_title, concept_body]
+      lesson_list.append(concept_list)
+      text = text[next_concept_end+len(CONCEPT_END)+2:]
+      if text.find(LESSON_KEY) == 0:
+        stage_list.append(lesson_list)
+        lesson_list = []
+        lesson_ready = True
+      else:
+        lesson_ready = False
+  stage_list.append(lesson_list)
+  return stage_list
