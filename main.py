9f2ddae967b75c0c7c82ae184bf75c6eb1b0c8f6
jesh <jes.hunsballe@gmail.com>
2015-08-13 22:59:47 +0200

    Fully functional and commented code. Users can wallpost/comment and GAE Datastore is working.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@
 # - get user from the post form input
 
 
-import cgi
 import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -20,7 +19,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-# Jinja and Google App Engine requirements
+# Jinja and Google App Engine libraries and environment variables
 import os
 import webapp2
 import jinja2
@@ -29,6 +28,8 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
   loader = jinja2.FileSystemLoader(template_dir),
   autoescape = True)
+
+# -------------------- GOOGLE APP ENGINE WEBPAGE HANDLERS --------------------
 
 # Google App Engine classes
 class Handler(webapp2.RequestHandler):
@@ -48,7 +49,6 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
   def get(self):
-    # self.write("<a href=""/notes""><h2>Click here to see notes</h2><a>")
     p = self.request.get('p')
     self.render("root_welcome.html", p = p)
 
@@ -77,9 +77,9 @@ class FizzBuzzHandler(Handler):
     p = p and abs(int(p))
     self.render('fizzbuzz.html', f = f, p = p)
 
-# DEFAULT_WALL = 'Public'
+# ---------- COMMENT/WALLPOST FUNCTIONALITY USING GAE DATASTORE ----------
 
-# Wall post/comment classes with Google App Engine Database
+# Wall post/comment classes with Google App Engine Datastore
 # We set a parent key on the 'Post' to ensure that they are all
 # in the same entity group. Queries across the single entity group
 # will be consistent.  However, the write rate should be limited to
@@ -92,12 +92,11 @@ def wall_key(wall_name):
 
 # These are the objects that will represent our Author and our Post. We're using
 # Object Oriented Programming to create objects in order to put them in Google's
-# Database. These objects inherit Googles ndb.Model class.
+# Datastore. These objects inherit Googles ndb.Model class.
 class Author(ndb.Model):
   """Sub model for representing an author."""
-  # identity = ndb.StringProperty(indexed=True)
+  # I'll just keep it simple and store a user name
   name = ndb.StringProperty(indexed=False)
-  # email = ndb.StringProperty(indexed=False)
 
 class Post(ndb.Model):
   """A main model for representing an individual post entry."""
@@ -109,51 +108,26 @@ class WallPage(Handler):
   def get(self):
     p = self.request.get('p','')
     wall_name = self.request.get('lesson')
-    # wall_name = self.request.get('wall_name',DEFAULT_WALL)
-    # if wall_name == DEFAULT_WALL.lower(): wall_name = DEFAULT_WALL
-    # Ancestor Queries, as shown here, are strongly consistent
-    # with the High Replication Datastore. Queries that span
-    # entity groups are eventually consistent. If we omitted the
-    # ancestor from this query there would be a slight chance that
-    # Wall Post that had just been written would not show up in a
-    # query.
-
     # [START query]
     posts_query = Post.query(ancestor = wall_key(wall_name)).order(-Post.date)
-    # The function fetch() returns all posts that satisfy our query. The function returns a list of
-    # post objects
+    # fetch() returns all posts that satisfy our query and returns a list of post objects
     posts = posts_query.fetch()
     # [END query]
-
-    # If a person is logged into Google's Services
-    # user = users.get_current_user()
-    # if user:
-    #     url = users.create_logout_url(self.request.uri)
-    #     url_linktext = 'Logout'
-    #     user_name = user.nickname()
-    # else:
-    #     url = users.create_login_url(self.request.uri)
-    #     url_linktext = 'Login'
-    #     user_name = 'Anonymous Poster'
-
-    # Create our posts html
+    # Create the posts html to print on the comments pages
     posts_html = ''
     for post in posts:
-
-      # Check if the current signed in user matches with the author's identity from this particular
-      # post. Newline character '\n' tells the computer to print a newline when the browser is
-      # is rendering our HTML
-      # if user and user.user_id() == post.author.identity:
-      #   posts_html += '<div><h3>(You) ' + post.author.name + '</h3>\n'
-      # else:
-      posts_html += '<div><h4>On ' + str(post.date.strftime('%d-%b-%Y')) + ' ' + post.author.name + ' wrote:</h4>\n'
+      # Newline character '\n' tells the computer to print a newline when the browser is
+      # rendering the html.
+      # We'll build a post consisting of date, time, user name and comment with html allowed:
+      posts_html += '<div><h4>On ' + str(post.date.strftime('%d-%b-%Y %H:%M')) + ' ' + post.author.name + ' wrote:</h4>\n'
       posts_html += '<blockquote>' + post.content + '</blockquote>\n'
       posts_html += '</div>\n'
-
+    # These parameters are passed to the input form on the comments page so the form in turn
+    # can include those parameters to the Post request. The lesson-wall gets carried over this way
+    # from notes --> comments --> Post and is the ancestor key for each Datastore entity (=wall)
     sign_query_params = urllib.urlencode({'wall_lesson': wall_name})
-
-    # Write Out Page here
-    self.render("wallbook.html",
+    # Write out the comments page here
+    self.render("commentbook.html",
                 p = p,
                 lesson = wall_name,
                 params = sign_query_params,
@@ -161,28 +135,26 @@ class WallPage(Handler):
 
 class PostWall(webapp2.RequestHandler):
   def post(self):
-    # We set the same parent key on the 'Post' to ensure each
-    # Post is in the same entity group. Queries across the
-    # single entity group will be consistent. However, the write
-    # rate to a single entity group should be limited to
-    # ~1/second.
+    # Same parent key on the 'Post' to ensure each Post is in the same entity group.
+    # Queries across the single entity group will be consistent.
+    #
+    # Pluck the lesson and username when sent here from the notes page
+    # and create post using the lesson name as Key
     wall_name = self.request.get('wall_lesson')
     post = Post(parent=wall_key(wall_name))
     user_name = self.request.get('user')
-    # When the person is making the post, check to see whether the person
-    # is logged into Google
-    # if users.get_current_user():
-    #   post.author = Author(name=users.get_current_user().nickname())
-    # else:
-      # post.author = Author(name='A. Nonymous')
     post.author = Author(name=user_name)
     # Get the content from our request parameters, in this case, the message
-    # is in the parameter 'content'
+    # is in the parameter 'comment' from the user submission form
     post.content = self.request.get('comment')
-    # Write to the Google Database
+    # Write to the Google Datastore
     post.put()
-    # Do other things here such as a page redirect
-    self.redirect('/comment?wall_name=' + wall_name)
+    # And finally a page redirect back to the comments page with same lesson set as the "wall"
+    # This way the user can immediately see his/her new comment added to the top.
+    # (It's the lesson-wall ancestor key circling back)
+    self.redirect('/comment?lesson=' + wall_name)
+
+# --------------- GOOGLE APP ENGINE PARAMETERS FOR WEBPAGE HANDLERS ---------------
 
 # Part of the Google App Engine code together with MainPage class. Recognizes
 # a certain path structure (e.g. '/') and uses the matching class for response.
@@ -193,8 +165,9 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/sign', PostWall),
                               ], debug=True)
 
+# -------- HELPER FUNCTIONS TO READ RAW STAGE NOTE TEMPLATE AND RETURN LIST --------
 
-# Helper functions to read raw stage note template and return list
+# Keys that _must_ be used in notes files to mark the content
 LESSON_KEY, CONCEPT_KEY, CONCEPT_END = '// LESSON //', '// CONCEPT //', '// CONCEPT END //'
 
 def load_stage(file_name):
@@ -205,34 +178,35 @@ def load_stage(file_name):
   # We use the String.join technique to join all of our elements in the list
   return ''.join(lines)
 
+# To be optimized later for 1) less clunkiness, 2) validated input, 3) search by regular
+# expressions, 4) storage by OOP (stage <-- lesson <-- concept objects)
 def make_lesson_list(text):
-  '''Takes the total lessons with concepts text, returns a nested list containing Lesson, Concept Title and Concept Description, [['L1', ['C1', 'D']], ['L2', ['C2', 'D']]]
-    To be optimized later for less clunkiness...
+  '''Takes text file containing all lessons with concepts text, returns a nested list containing Lesson, Concept Title and Concept Description, [['L1', ['C1', 'D']], ['L2', ['C2', 'D']],...]
   '''
-  stage_list = []
-  lesson_list = []
-  lesson_ready = True
-  while text != '':
-    next_lesson_start = text.find(LESSON_KEY) + len(LESSON_KEY)+1
-    next_lesson_end = text.find(CONCEPT_KEY)-1
-    next_concept_start = text.find(CONCEPT_KEY) + len(CONCEPT_KEY)+1
+  stage_list = [] # Container list for final output
+  lesson_list = [] # Intermediate container list for lesson contents
+  lesson_ready = True # Gatekeeper for closing/opening a fresh intermediate lesson container
+  while text != '': # Loop until the complete text of lessons+concepts is chipped down to nothing
+    next_lesson_start = text.find(LESSON_KEY) + len(LESSON_KEY)+1 # Find start index of next lesson
+    next_lesson_end = text.find(CONCEPT_KEY)-1 # ...and end index
+    next_concept_start = text.find(CONCEPT_KEY) + len(CONCEPT_KEY)+1 # Same procedure for concepts
     next_concept_end = text.find(CONCEPT_END)-1
-    if (next_lesson_end >= 0 and lesson_ready == True):
-      lesson = text[next_lesson_start:next_lesson_end]
-      lesson_list.append(lesson)
-      text = text[next_lesson_end+1:]
-    elif next_concept_end >= 0:
-      concept_str = text[next_concept_start:next_concept_end]
-      concept_title = concept_str[:concept_str.find('\n')]
-      concept_body = concept_str[concept_str.find('\n')+1:]
-      concept_list = [concept_title, concept_body]
-      lesson_list.append(concept_list)
-      text = text[next_concept_end+len(CONCEPT_END)+2:]
-      if text.find(LESSON_KEY) == 0:
-        stage_list.append(lesson_list)
-        lesson_list = []
-        lesson_ready = True
-      else:
-        lesson_ready = False
-  if stage_list != []: stage_list.append(lesson_list)
-  return stage_list
+    if (next_lesson_end >= 0 and lesson_ready == True): # If gate is open, it's time for new lesson
+      lesson = text[next_lesson_start:next_lesson_end] # Snip out the lesson headline
+      lesson_list.append(lesson) # Store headline in intermediate lesson list
+      text = text[next_lesson_end+1:] # Shorten text by lesson key + lesson headline
+    elif next_concept_end >= 0: # If gate was closed, it's time to add concepts to the lesson list
+      concept_str = text[next_concept_start:next_concept_end] # Snip out the next complete concept
+      concept_title = concept_str[:concept_str.find('\n')] # Concept title is first line until CR (\n)
+      concept_body = concept_str[concept_str.find('\n')+1:] # Concept description is the rest
+      concept_list = [concept_title, concept_body] # Wrap the concept in it's own little list
+      lesson_list.append(concept_list) # Add that little concept list to the current lesson list
+      text = text[next_concept_end+len(CONCEPT_END)+2:] # Shorten text again to cut away last concept
+      if text.find(LESSON_KEY) == 0: # Look what's next in the text. If new lesson is up, it's time to...
+        stage_list.append(lesson_list) # Wrap latest intermediate lesson list in stage list
+        lesson_list = [] # Clear the intermediate lesson list, so ready for next lesson
+        lesson_ready = True # Tell gatekeeper a new lesson is coming up
+      else: # If the next in the text wasn't a new lesson, then keep finding concepts
+        lesson_ready = False # To find concepts, the gatekeeper must know we're not ready for a new lesson
+  if stage_list != []: stage_list.append(lesson_list) # Put last lesson list into the stage list
+  return stage_list # Time to return the nicely wrapped up lessons + concepts
